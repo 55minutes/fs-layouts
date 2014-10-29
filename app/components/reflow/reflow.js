700036b239b3621946f0e774204a4bed863580e9
George Song <george@55minutes.com>
2014-10-29 13:20:41 -0700

    Checkpoint for product detail

--- app/components/reflow/reflow.js
+++ app/components/reflow/reflow.js
@@ -5,24 +5,36 @@
 angular.module('reflow', [])
 .factory('reflow', reflow);
 
-
 function reflow() {
   var factory = {};
 
   var cart = $('#cart');
   var content = $('#main-content');
 
-  factory.reflow = function(width) {
+  factory.products = {};
+
+  factory.products.reflow = function(width) {
     var basis = 100 / Math.floor(width / 320);
     $('.tile').css('flex', '0 0 ' + basis + '%');
     $('.tile-2x').css('flex', '0 0 ' + basis * 2 + '%');
   };
 
-  factory.register = function() {
+  factory.products.register = function() {
     $(window).bind('resize', function() {
-      factory.reflow(content.width());
+      factory.products.reflow(content.width());
     });
 
+    cart.watch({
+      properties: 'width',
+      callback: function(data, i) {
+        factory.products.reflow(content.width());
+      }
+    });
+  };
+
+  factory.productDetail = {};
+
+  factory.productDetail.register = function() {
     cart.watch({
       properties: 'width',
       callback: function(data, i) {
