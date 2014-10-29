01e9bc37b185ad7f82faf86384daae74a8e2c4ab
George Song <george@55minutes.com>
2014-10-29 05:59:17 -0700

    We have real products

--- app/components/fixtures/fixtures.js
+++ app/components/fixtures/fixtures.js
@@ -15,7 +15,39 @@ function fixtures() {
       width: 3.5,
       image: 'business-card_slim.png',
       backgroundColor: '#f5ca26',
-      backgroundColorHover: '#DCB522'
+      backgroundColorHover: '#dcb522',
+    }, {
+      name: 'Business Card',
+      type: 'Classic',
+      height: 2,
+      width: 3.5,
+      image: 'business-card_classic.png',
+      backgroundColor: '#ffbe1d',
+      backgroundColorHover: '#e5aa1a',
+    }, {
+      name: 'Business Card',
+      type: 'Rounded',
+      height: 2,
+      width: 3.5,
+      image: 'business-card_rounded.png',
+      backgroundColor: '#ffb111',
+      backgroundColorHover: '#e59f0f',
+    }, {
+      name: 'Postcard',
+      type: 'Classic',
+      height: 4,
+      width: 6,
+      image: 'postcard_classic.png',
+      backgroundColor: '#fd7e00',
+      backgroundColorHover: '#e37100',
+    }, {
+      name: 'Postcard',
+      type: 'Medium',
+      height: 5,
+      width: 7,
+      image: 'postcard_medium.png',
+      backgroundColor: '#ef6a21',
+      backgroundColorHover: '#d65f1d',
     }];
 
     return factory;
--- app/sections/product/overview.js
+++ app/sections/product/overview.js
@@ -2,12 +2,36 @@
 
 'use strict';
 
-angular.module('productOverview', ['reflow'])
+angular.module('productOverview', ['fixtures', 'reflow'])
 .controller('ProductOverviewController', ProductOverviewController);
 
-function ProductOverviewController($scope, reflow) {
+function initialReflow(reflow) {
+  var target = document.querySelector('main');
+
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      reflow.reflow($('#main-content').width());
+    });
+  });
+
+  var config = {childList: true, subtree: true};
+
+  observer.observe(target, config);
+}
+
+function ProductOverviewController($scope, fixtures, reflow) {
+  $scope.tileHover = function(e, color) {
+    $(e.target).css('background-color', color);
+  };
+
+  $scope.stop = function(e) {
+    e.stopPropagation();
+  }
+
+  $scope.products = fixtures.products;
+
   reflow.register();
-  reflow.reflow($('#main-content').width());
+  initialReflow(reflow);
 }
 
 })(window, window.angular, window.jQuery);
